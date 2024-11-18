cacfa280a998cb39c9ed039b7b261f20ebe4643e
Bel Cogo <bel.cogo@sap.com>
2024-11-17 22:28:11 -0300

    WIP: add graphs

--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
 
 # Método para obter o valor numérico de GeneticRisk
 def get_formated_genetic_risk(risk):
@@ -24,7 +28,112 @@ data_analyses_on = st.toggle('Exibir análise dos dados')
 if data_analyses_on:
     st.header('Dados de Pacientes com Câncer')
 
-    # TODO: Mostrar os gráficos;
+    dataset = pd.read_csv('./src/resources/The_Cancer_data_1500_V2.csv')
+    dataset_for_plots = dataset.copy()
+    dataset_for_plots['GeneticRisk'] = dataset_for_plots['GeneticRisk'].map({0: 'Baixo', 1: 'Médio', 2: 'Alto'})
+    dataset_for_plots['Gender'] =  dataset_for_plots['Gender'].map({0: 'Masculino', 1: 'Feminino'})
+    dataset_for_plots['Smoking'] =  dataset_for_plots['Smoking'].map({0: 'Não', 1: 'Sim'})
+    dataset_for_plots['CancerHistory']  = dataset_for_plots['CancerHistory'].map({0: 'Sem histórico', 1: 'Com histórico'})
+
+    plot = sns.countplot(x=dataset_for_plots['Gender'])
+
+    plt.title('Distribuição por Gênero')
+    plt.ylabel('Contagem')
+    plt.xlabel('Gênero')
+
+    st.pyplot(plot.get_figure())
+
+    plt.clf()
+
+    plot = sns.countplot(data=dataset_for_plots, x='Gender', hue='Diagnosis')
+
+    plt.title('Relação entre Gênero e Câncer')
+    plt.xlabel('Gênero')
+    plt.ylabel('Contagem')
+    plt.legend(title='Diagnosticado com Câncer', labels=['Não', 'Sim'])
+
+    st.pyplot(plot.get_figure())
+
+    plt.clf()
+
+    plot = sns.countplot(x=dataset_for_plots['Smoking'])
+
+    plt.title('Relação entre Fumantes e Câncer')
+    plt.xlabel('Fumante')
+    plt.ylabel('Contagem')
+
+    st.pyplot(plot.get_figure())
+
+    plt.clf()
+
+    plot = sns.countplot(data=dataset_for_plots, x='Smoking', hue='Diagnosis')
+
+    plt.title('Relação entre Fumantes e Câncer')
+    plt.xlabel('Fumante')
+    plt.ylabel('Contagem')
+    plt.legend(title='Diagnosticado com Câncer', labels=['Não', 'Sim'])
+
+    st.pyplot(plot.get_figure())
+
+    plt.clf()
+
+    genetic_risk_distribution = np.unique(dataset_for_plots['GeneticRisk'], return_counts=True)
+    divisor = len(dataset_for_plots) * 100
+
+    high_risk_perc = genetic_risk_distribution[1][0] / divisor
+    low_risk_perc = genetic_risk_distribution[1][1] / divisor
+    medium_risk_perc = genetic_risk_distribution[1][2] / divisor
+
+    sizes = [low_risk_perc, medium_risk_perc, high_risk_perc]
+    labels = ["Baixo Risco", "Médio Risco", "Alto Risco"]
+
+    fig = plt.figure(figsize=(8, 6))
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
+    plt.axis('equal')
+    plt.title('Distribuição de Pacientes por Risco Genético')
+    plt.show()
+
+    st.pyplot(fig)
+
+    plt.clf()
+
+    plot = sns.countplot(data=dataset_for_plots, x='GeneticRisk', hue='Diagnosis')
+
+    plt.title('Relação entre Risco Genêtico e Câncer')
+    plt.xlabel('Risco Genêtico')
+    plt.ylabel('Contagem')
+    plt.legend(title='Diagnosticado com Câncer', labels=['Não', 'Sim'])
+
+    st.pyplot(plot.get_figure())
+
+    plt.clf()
+
+    plot = sns.violinplot(x='Diagnosis', y='AlcoholIntake', data=dataset_for_plots)
+
+    plt.title('Relação entre Ingestão de Álcool e Câncer')
+    plt.xlabel('Diagnosticado com Câncer')
+    plt.ylabel('Ingestão de Álcool (Unidades por Semana)')
+
+    st.pyplot(plot.get_figure())
+
+    plt.clf()
+
+    plot = sns.swarmplot(x='Age', hue='Diagnosis', data=dataset_for_plots)
+
+    plt.title('Relação entre Idade e Câncer')
+    plt.xlabel('Idade')
+    plt.ylabel('Diagnosticado com Câncer')
+    plt.legend(title='Diagnosticado com Câncer', labels=['Sim', 'Não'])
+
+    st.pyplot(plot.get_figure())
+
+    plt.clf()
+
+    patients_data_for_tree_map = dataset_for_plots.drop(columns=['BMI', 'Age', 'AlcoholIntake', 'PhysicalActivity'])
+
+    graph = px.treemap(patients_data_for_tree_map, path=['GeneticRisk', 'CancerHistory','Smoking' , 'Gender'], color="Diagnosis", color_continuous_scale=['#32a852', '#3261a8', '#a83259'])
+    st.write('Treemap renderizado em nova aba.')
+    graph.show()
 
 
 st.header('Predição de Diagnóstico')
